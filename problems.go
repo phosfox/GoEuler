3767d9a2c4ec8165ca3595f80a23aa0e37ce8f95
Constantin <constantin.wentzek@student-fhws.de>
2018-09-20 17:43:07 +0200

    changes

--- problems.go
+++ problems.go
@@ -25,7 +25,7 @@ func problem24() {
 	*/
 	// https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
 	array := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	for i := 1; i <= 10; i++ {
+	for i := 1; i <= 1000000; i++ {
 		fmt.Println(array)
 		nextPermutation(array)
 	}
