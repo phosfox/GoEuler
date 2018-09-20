5134fddb3c5eda0ad1a2c196a621447a7b574955
Constantin <constantin.wentzek@student-fhws.de>
2018-09-20 17:23:08 +0200

    problem 24 solved

--- problems.go
+++ problems.go
@@ -13,8 +13,60 @@ import (
 )
 
 func main() {
-	problem23()
+	problem24()
 
+}
+
+func problem24() {
+	/*
+		A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+									012   021   102   120   201   210
+		What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+	*/
+	array := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	for i := 1; i <= 10; i++ {
+		fmt.Println(array)
+		nextPermutation(array)
+	}
+}
+
+func nextPermutation(array []int) bool {
+	//Find longest non-increasing suffix
+	i := len(array) - 1
+	for i > 0 && array[i-1] >= array[i] {
+		//Now i is the head index of the suffix
+		i--
+	}
+	//Are we at  the last permutation already?
+	if i <= 0 {
+		return false
+	}
+	//Let array[i - 1] be the pivot
+	//Find rightmost element that exceeds the pivot
+	j := len(array) - 1
+	for array[j] <= array[i-1] {
+		j--
+	}
+	//Now the value array[j] will become the new pivot
+	//Assertion: j >= i
+
+	//Swap the pivot with j
+	temp := array[i-1]
+	array[i-1] = array[j]
+	array[j] = temp
+
+	//Reverse the suffix
+	j = len(array) - 1
+	for i < j {
+		temp = array[i]
+		array[i] = array[j]
+		array[j] = temp
+		i++
+		j--
+
+	}
+	//Successfully computed the next permutation
+	return true
 }
 
 func problem23() {
