92b0ab585f19da0d63b2ff5e1828e2810e8d4624
phosfox <mordredur@googlemail.com>
2018-09-21 16:33:20 +0200

    problem25 solved

--- problems.go
+++ problems.go
@@ -13,8 +13,48 @@ import (
 )
 
 func main() {
-	problem23()
+	problem25()
 
+}
+
+func problem25() {
+	/*
+		The Fibonacci sequence is defined by the recurrence relation:
+		    Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+		Hence the first 12 terms will be:
+		    F1 = 1
+		    F2 = 1
+		    F3 = 2
+		    F4 = 3
+		    F5 = 5
+		    F6 = 8
+		    F7 = 13
+		    F8 = 21
+		    F9 = 34
+		    F10 = 55
+		    F11 = 89
+		    F12 = 144
+		The 12th term, F12, is the first term to contain three digits.
+		What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+	*/
+	var limit big.Int
+	limit.Exp(big.NewInt(10), big.NewInt(999), nil)
+	fmt.Println(getFib(&limit))
+
+}
+
+func getFib(limit *big.Int) int {
+	a := big.NewInt(0)
+	b := big.NewInt(1)
+	counter := 0
+	for a.Cmp(limit) < 0 {
+		counter++
+		a.Add(a, b)
+		// Swap a and b so that b is the next number in the sequence.
+		a, b = b, a
+	}
+	fmt.Println(a)
+	return counter
 }
 
 func problem23() {
